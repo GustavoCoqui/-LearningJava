94dcf20b0603055458bd803a7e0057d538478aae
gustavo coqui barbosa <gustavo.coqui@yahoo.com.br>
2022-08-10 16:27:50 -0300

    First matriz program!

---
+++ ExercicioSobreMatriz/src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Qual é a ordem da matriz?");
+		int n = sc.nextInt();
+		
+		
+		int[][] matriz = new int[n][n];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = sc.nextInt();
+			}
+			
+		}
+		
+		System.out.println("Diagonal principal:");
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.println(matriz[i][i] + " ");
+			
+		}
+		System.out.println();
+
+		
+		int count = 0;
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j] < 0) {
+					count++;
+				}
+				
+			}
+			
+		}
+		
+		System.out.println("Números negativos:"+count);
+		
+		
+		sc.close();
+		
+		
+
+	}
+
+}
